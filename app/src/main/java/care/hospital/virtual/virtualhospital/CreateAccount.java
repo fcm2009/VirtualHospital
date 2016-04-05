bf8d410d4dd1b43207ca6148d59d508f422102e5
fcm2009 <fcm2009@gmail.com>
2016-04-05 22:17:55 +0300

    create and login

--- app/src/main/java/care/hospital/virtual/virtualhospital/CreateAccount.java
+++ app/src/main/java/care/hospital/virtual/virtualhospital/CreateAccount.java
@@ -6,9 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import org.json.JSONException;
